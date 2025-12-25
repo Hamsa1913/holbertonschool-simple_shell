7c9e5eff2eed2a979924e5e1ba122965532d6b6e
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-12-25 22:07:56 +0000

    adding path

--- path.c
+++ path.c
@@ -9,37 +9,37 @@
 char *find_path(char *cmd)
 {
 	int i;
-	char *path = NULL;
+	char *path;
 	char *path_copy;
 	char *token;
 	char *full;
 
-	if (cmd[0] == '/')
+	if (cmd[0] == '/' || (cmd[0] == '.' && cmd[1] == '/'))
 		return (_strdup(cmd));
 
 	for (i = 0; environ[i]; i++)
 	{
 		if (_strncmp(environ[i], "PATH=", 5) == 0)
-		{
 			path = environ[i] + 5;
-			break;
-		}
 	}
-
-	if (!path)
-		return (NULL);
 
 	path_copy = _strdup(path);
 	token = strtok(path_copy, ":");
 
 	while (token)
 	{
-		full = build_path(token, cmd);
+		full = malloc(_strlen(token) + _strlen(cmd) + 2);
+		if (!full)
+			return (NULL);
+
+		sprintf(full, "%s/%s", token, cmd);
+
 		if (access(full, X_OK) == 0)
 		{
 			free(path_copy);
 			return (full);
 		}
+
 		free(full);
 		token = strtok(NULL, ":");
 	}
