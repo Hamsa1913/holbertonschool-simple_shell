d62e286b89b2fb84b34aa384a489e724b52faf2c
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-12-25 10:04:52 +0000

    adding path finder

--- simple_shell.h
+++ simple_shell.h
@@ -1,12 +1,14 @@
 #ifndef SIMPLE_SHELL_H
 #define SIMPLE_SHELL_H
 
+#include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <stdio.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 
+extern char **environ;
+
 int hsh_loop(void);
+char *find_path(char *command);
 
 #endif
