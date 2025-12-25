28d74eeeab9fdd2ac0d9122aed165aa2e1efb882
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-12-25 22:12:44 +0000

    fixing variables

--- utils.c
+++ utils.c
@@ -1,59 +1,55 @@
 #include "simple_shell.h"
 
 /**
- * _strlen - get length of string
- * @s: string
- *
- * Return: length
+ * trim_spaces - removes leading and trailing spaces
  */
+char *trim_spaces(char *str)
+{
+	while (*str == ' ' || *str == '\t')
+		str++;
+
+	if (*str == '\0')
+		return (str);
+
+	char *end = str + _strlen(str) - 1;
+	while (end > str && (*end == ' ' || *end == '\t'))
+		end--;
+
+	end[1] = '\0';
+	return (str);
+}
+
+char **split_line(char *line)
+{
+	char **argv;
+	int i = 0;
+	char *token;
+
+	argv = malloc(sizeof(char *) * 64);
+	if (!argv)
+		return (NULL);
+
+	token = strtok(line, " ");
+	while (token)
+	{
+		argv[i++] = token;
+		token = strtok(NULL, " ");
+	}
+	argv[i] = NULL;
+	return (argv);
+}
+
 int _strlen(char *s)
 {
 	int i = 0;
-
-	while (s[i])
+	while (s && s[i])
 		i++;
-
 	return (i);
 }
 
-/**
- * _strdup - duplicate a string
- * @str: string
- *
- * Return: pointer to new string
- */
-char *_strdup(char *str)
+int _strncmp(char *s1, char *s2, int n)
 {
-	int i, len;
-	char *dup;
-
-	if (!str)
-		return (NULL);
-
-	len = _strlen(str);
-	dup = malloc(sizeof(char) * (len + 1));
-	if (!dup)
-		return (NULL);
-
-	for (i = 0; i < len; i++)
-		dup[i] = str[i];
-
-	dup[i] = '\0';
-	return (dup);
-}
-
-/**
- * _strncmp - compare strings
- * @s1: string 1
- * @s2: string 2
- * @n: bytes
- *
- * Return: difference
- */
-int _strncmp(char *s1, char *s2, size_t n)
-{
-	size_t i;
-
+	int i;
 	for (i = 0; i < n; i++)
 	{
 		if (s1[i] != s2[i])
@@ -61,56 +57,27 @@ int _strncmp(char *s1, char *s2, size_t n)
 		if (s1[i] == '\0')
 			return (0);
 	}
-
 	return (0);
 }
 
-/**
- * build_path - build full path
- * @dir: directory
- * @cmd: command
- *
- * Return: full path
- */
-char *build_path(char *dir, char *cmd)
+char *_strdup(char *s)
 {
-	int len;
-	char *path;
+	char *dup;
+	int len = _strlen(s), i;
 
-	len = _strlen(dir) + _strlen(cmd) + 2;
-	path = malloc(sizeof(char) * len);
-	if (!path)
+	dup = malloc(len + 1);
+	if (!dup)
 		return (NULL);
 
-	strcpy(path, dir);
-	strcat(path, "/");
-	strcat(path, cmd);
+	for (i = 0; i <= len; i++)
+		dup[i] = s[i];
 
-	return (path);
+	return (dup);
 }
 
-/**
- * trim_spaces - remove leading and trailing spaces
- * @str: string to trim
- *
- * Return: pointer to trimmed string
- */
-char *trim_spaces(char *str)
+void _memcpy(char *dest, char *src, int n)
 {
-	char *end;
-
-	while (*str == ' ' || *str == '\t')
-		str++;
-
-	if (*str == '\0')
-		return (str);
-
-	end = str + _strlen(str) - 1;
-	while (end > str && (*end == ' ' || *end == '\t'))
-		end--;
-
-	*(end + 1) = '\0';
-
-	return (str);
+	int i;
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
 }
-
