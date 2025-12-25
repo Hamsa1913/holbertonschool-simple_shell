4050841a96ae2f5dc13630db9871bfb4485a3067
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-12-25 22:04:30 +0000

    fixing for the milion time

--- hsh.c
+++ hsh.c
@@ -1,22 +1,16 @@
 #include "simple_shell.h"
 
 /**
- * hsh_loop - main shell loop
- *
- * Return: 0
+ * hsh_loop - shell loop
  */
 int hsh_loop(void)
 {
-	char *line;
-	size_t len;
+	char *line = NULL, *cmd, *path;
+	size_t len = 0;
 	ssize_t r;
 	pid_t pid;
 	int status;
-	char *argv[2];
-	char *cmd_path;
-
-	line = NULL;
-	len = 0;
+	char **argv;
 
 	while (1)
 	{
@@ -33,31 +27,39 @@ int hsh_loop(void)
 		if (line[r - 1] == '\n')
 			line[r - 1] = '\0';
 
-		if (*line == '\0')
+		cmd = trim_spaces(line);
+
+		if (*cmd == '\0')
 			continue;
 
-		cmd_path = find_path(line);
-		if (cmd_path == NULL)
+		_memcpy(line, cmd, _strlen(cmd) + 1);
+
+		argv = split_line(line);
+		if (!argv)
+			continue;
+
+		path = find_path(argv[0]);
+		if (!path)
 		{
 			write(STDERR_FILENO, "./hsh: 1: ", 10);
-			write(STDERR_FILENO, line, _strlen(line));
+			write(STDERR_FILENO, argv[0], _strlen(argv[0]));
 			write(STDERR_FILENO, ": not found\n", 12);
+			free(argv);
 			continue;
 		}
-
-		argv[0] = cmd_path;   /* fixing this line and adding null */
-		argv[1] = NULL;
 
 		pid = fork();
 		if (pid == 0)
 		{
-			execve(cmd_path, argv, environ);
+			execve(path, argv, environ);
 			exit(1);
 		}
 		else
+		{
 			wait(&status);
-
-		free(cmd_path);
+			free(path);
+			free(argv);
+		}
 	}
 }
 
