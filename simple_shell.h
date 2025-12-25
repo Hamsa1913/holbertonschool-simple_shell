1e4d1eed4013ac7c52d78fcfac4159cd62f5e0d9
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-12-25 21:18:50 +0000

    adding 3 utility functions

--- simple_shell.h
+++ simple_shell.h
@@ -5,10 +5,22 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
 
 extern char **environ;
 
+/* main loop function */
 int hsh_loop(void);
-char *find_path(char *command);
+
+/* path functions */
+char *find_path(char *cmd);
+char *build_path(char *dir, char *cmd);
+
+/* utility file functions becuse i cant used more than 5 functions */
+int _strlen(char *s);
+char *_strdup(char *str);
+int _strncmp(char *s1, char *s2, size_t n);
 
 #endif
