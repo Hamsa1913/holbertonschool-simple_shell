256916dc86f2d81b6e62adb697392497dc5e8fb8
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-12-25 21:12:03 +0000

    environ loop

--- path.c
+++ path.c
@@ -1,70 +1,43 @@
 #include "simple_shell.h"
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
 
 /**
- * get_path_from_environ - finds PATH variable in environ
+ * find_path - locate command in PATH
+ * @cmd: command name
  *
- * Return: pointer to PATH string, or NULL if not found
+ * Return: full path or NULL
  */
-char *get_path_from_environ(void)
+char *find_path(char *cmd)
 {
 	int i;
-	char *prefix = "PATH=";
-	size_t len = strlen(prefix);
+	char *path;
+	char *path_copy;
+	char *token;
+	char *full;
 
-	i = 0;
-	while (environ[i])
+	if (cmd[0] == '/')
+		return (_strdup(cmd));
+
+	for (i = 0; environ[i]; i++)
 	{
-		if (strncmp(environ[i], prefix, len) == 0)
-			return (environ[i] + len);
-		i++;
+		if (_strncmp(environ[i], "PATH=", 5) == 0)
+			path = environ[i] + 5;
 	}
-	return (NULL);
-}
 
-/**
- * find_path - finds full path of command in PATH
- * @command: command name
- *
- * Return: malloc'd full path if found, else NULL
- */
-char *find_path(char *command)
-{
-	char *path_env, *path_copy, *token;
-	char *full_path;
-	size_t len;
-
-	path_env = get_path_from_environ();
-	if (!path_env)
-		return (NULL);
-
-	path_copy = strdup(path_env);
-	if (!path_copy)
-		return (NULL);
-
+	path_copy = _strdup(path);
 	token = strtok(path_copy, ":");
+
 	while (token)
 	{
-		len = strlen(token) + strlen(command) + 2;
-		full_path = malloc(len);
-		if (!full_path)
-			break;
-
-		sprintf(full_path, "%s/%s", token, command);
-
-		if (access(full_path, X_OK) == 0)
+		full = build_path(token, cmd);
+		if (access(full, X_OK) == 0)
 		{
 			free(path_copy);
-			return (full_path);
+			return (full);
 		}
-
-		free(full_path);
+		free(full);
 		token = strtok(NULL, ":");
 	}
 
 	free(path_copy);
 	return (NULL);
 }
-
