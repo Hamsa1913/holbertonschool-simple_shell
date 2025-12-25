2f7fad14429dfc03f98cb4c958acb6e3787e1029
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-12-25 10:35:54 +0000

    adding loop for path before fork loop

--- hsh.c
+++ hsh.c
@@ -1,59 +1,73 @@
 #include "simple_shell.h"
+#include <string.h>
+#include <stdio.h>
 
 /**
- * hsh_loop - Simple shell loop
+ * hsh_loop - Simple shell main loop
  *
  * Return: 0 on success
  */
 int hsh_loop(void)
 {
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t r;
-	pid_t pid;
-	int status;
-	char *argv[2];
+        char *line = NULL;
+        size_t len = 0;
+        ssize_t r;
+        pid_t pid;
+        int status;
+        char *argv[2];
 
-	while (1)
-	{
-		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, "#cisfun$ ", 9);
+        while (1)
+        {
+                if (isatty(STDIN_FILENO))
+                        write(STDOUT_FILENO, "($) ", 4);
 
-		r = getline(&line, &len, stdin);
-		if (r == -1)
-		{
-			free(line);
-			return (0);
-		}
+                r = getline(&line, &len, stdin);
+                if (r == -1)
+                {
+                        free(line);
+                        return (0);
+                }
 
-		if (r > 0 && line[r - 1] == '\n')
-			line[r - 1] = '\0';
+                if (r > 0 && line[r - 1] == '\n')
+                        line[r - 1] = '\0';
 
-		if (*line == '\0')
-			continue;
+                if (*line == '\0')
+                        continue;
 
-		argv[0] = line;
-		argv[1] = NULL;
+                /* this is for PATH handling */
+                char *cmd_path = find_path(line);
+                if (!cmd_path)
+                {
+                        fprintf(stderr, "./hsh: 1: %s: not found\n", line);
+                        continue;
+                }
 
-		pid = fork();
-		if (pid == -1)
-		{
-			perror("fork");
-			free(line);
-			return (0);
-		}
+                argv[0] = cmd_path;
+                argv[1] = NULL;
 
-		if (pid == 0)
-		{
-			if (execve(argv[0], argv, environ) == -1)
-			{
-				perror("./hsh");
-				exit(0);
-			}
-		}
-		else
-		{
-			wait(&status);
-		}
-	}
+                pid = fork();
+                if (pid == -1)
+                {
+                        perror("fork");
+                        free(line);
+                        free(cmd_path);
+                        return (1);
+                }
+
+                if (pid == 0)
+                {
+                        execve(argv[0], argv, environ);
+                        _exit(127);
+                }
+                else
+                {
+                        wait(&status);
+                }
+
+                free(cmd_path);
+        }
+
+        free(line);
+        return (0);
 }
+
