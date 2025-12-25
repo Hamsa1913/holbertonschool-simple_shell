b48fa15ab0c0a2d15d52ce9562203b37b8a22894
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-12-25 10:55:45 +0000

    avoid using gitenv and use environ

--- path.c
+++ path.c
@@ -1,47 +1,70 @@
 #include "simple_shell.h"
 #include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 /**
- * find_path - finds command in PATH
+ * get_path_from_environ - finds PATH variable in environ
+ *
+ * Return: pointer to PATH string, or NULL if not found
+ */
+char *get_path_from_environ(void)
+{
+	int i;
+	char *prefix = "PATH=";
+	size_t len = strlen(prefix);
+
+	i = 0;
+	while (environ[i])
+	{
+		if (strncmp(environ[i], prefix, len) == 0)
+			return (environ[i] + len);
+		i++;
+	}
+	return (NULL);
+}
+
+/**
+ * find_path - finds full path of command in PATH
  * @command: command name
  *
- * Return: full path or NULL
+ * Return: malloc'd full path if found, else NULL
  */
 char *find_path(char *command)
 {
-        char *path, *path_copy, *token;
-        char *full_path;
-        size_t len;
+	char *path_env, *path_copy, *token;
+	char *full_path;
+	size_t len;
 
-        path = getenv("PATH");
-        if (!path)
-                return (NULL);
+	path_env = get_path_from_environ();
+	if (!path_env)
+		return (NULL);
 
-        path_copy = strdup(path);
-        if (!path_copy)
-                return (NULL);
+	path_copy = strdup(path_env);
+	if (!path_copy)
+		return (NULL);
 
-        token = strtok(path_copy, ":");
-        while (token)
-        {
-                len = strlen(token) + strlen(command) + 2;
-                full_path = malloc(len);
-                if (!full_path)
-                        break;
+	token = strtok(path_copy, ":");
+	while (token)
+	{
+		len = strlen(token) + strlen(command) + 2;
+		full_path = malloc(len);
+		if (!full_path)
+			break;
 
-                sprintf(full_path, "%s/%s", token, command);
+		sprintf(full_path, "%s/%s", token, command);
 
-                if (access(full_path, X_OK) == 0)
-                {
-                        free(path_copy);
-                        return (full_path);
-                }
+		if (access(full_path, X_OK) == 0)
+		{
+			free(path_copy);
+			return (full_path);
+		}
 
-                free(full_path);
-                token = strtok(NULL, ":");
-        }
+		free(full_path);
+		token = strtok(NULL, ":");
+	}
 
-        free(path_copy);
-        return (NULL);
+	free(path_copy);
+	return (NULL);
 }
 
