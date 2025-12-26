49b17f675a4b0a35c85a7d31f0dbbaa392ae8069
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-12-26 00:23:19 +0000

    adding buffer for size_t

--- hsh.c
+++ hsh.c
@@ -2,71 +2,74 @@
 
 /**
  * hsh_loop - Main shell loop
- * Return: 0 on success
  */
-int hsh_loop(void)
+void hsh_loop(void)
 {
     char *line = NULL;
-    size_t len = 0;
-    ssize_t r;
-    pid_t pid;
-    int status;
+    size_t bufsize = 0;
+    ssize_t nread;
     char **argv;
     char *cmd_path;
+    pid_t pid;
+    int status;
 
     while (1)
     {
-        if (isatty(STDIN_FILENO))
-            write(STDOUT_FILENO, "#cisfun$ ", 9);
-
-        r = getline(&line, &len, stdin);
-        if (r == -1)
+        write(STDOUT_FILENO, ":) ", 3);
+        nread = getline(&line, &bufsize, stdin);
+        if (nread == -1)
         {
             free(line);
-            return (0);
+            exit(EXIT_SUCCESS);
         }
 
-        if (r > 0 && line[r - 1] == '\n')
-            line[r - 1] = '\0';
-
-        line = trim_spaces(line);
-        if (*line == '\0')
+        _trim_spaces(line);
+        if (_strlen(line) == 0)
             continue;
 
         argv = split_line(line);
-        if (!argv || !argv[0])
+        if (!argv[0])
         {
             free(argv);
             continue;
         }
 
         cmd_path = find_path(argv[0]);
+        if (!cmd_path)
+        {
+            write(STDERR_FILENO, argv[0], _strlen(argv[0]));
+            write(STDERR_FILENO, ": not found\n", 12);
+            for (int i = 0; argv[i]; i++)
+                free(argv[i]);
+            free(argv);
+            continue;
+        }
 
         pid = fork();
-        if (pid == -1)
-        {
-            perror("fork");
-            free(argv);
-            free(line);
-            return (0);
-        }
-
         if (pid == 0)
         {
-            if (execve(cmd_path ? cmd_path : argv[0], argv, environ) == -1)
-            {
-                perror("./hsh");
-                exit(EXIT_FAILURE);
-            }
+            execve(cmd_path, argv, environ);
+            perror("execve");
+            exit(EXIT_FAILURE);
         }
+        else if (pid > 0)
+            waitpid(pid, &status, 0);
         else
-            wait(&status);
+            perror("fork");
 
+        free(cmd_path);
+        for (int i = 0; argv[i]; i++)
+            free(argv[i]);
         free(argv);
-        if (cmd_path)
-            free(cmd_path);
     }
+}
 
-    free(line);
+/**
+ * main - Entry point
+ */
+int main(void)
+{
+    hsh_loop();
     return (0);
 }
+
