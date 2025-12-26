c64684061dbe9a073ab64c5ceb2c2a9d6f12e4e1
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-12-26 00:16:08 +0000

    fixing the while loop in stalen, and in memcpy

--- utils.c
+++ utils.c
@@ -1,39 +1,47 @@
 #include "simple_shell.h"
 
 /**
- * _strlen - get string length
+ * _strlen - Returns the length of a string
  */
-size_t _strlen(const char *s)
+int _strlen(const char *s)
 {
-    size_t i = 0;
-    while (s[i])
-        i++;
-    return i;
+    int len = 0;
+
+    while (s[len])
+        len++;
+    return (len);
 }
 
 /**
- * _strdup - duplicate string
+ * _strdup - Duplicates a string
  */
 char *_strdup(const char *s)
 {
-    size_t len = _strlen(s) + 1;
-    char *dup = malloc(len);
-    size_t i;
+    char *dup;
+    int len, i;
 
+    if (!s)
+        return (NULL);
+
+    len = _strlen(s);
+    dup = malloc(len + 1);
     if (!dup)
-        return NULL;
+        return (NULL);
 
     for (i = 0; i < len; i++)
         dup[i] = s[i];
-    return dup;
+    dup[len] = '\0';
+
+    return (dup);
 }
 
 /**
- * _strncmp - compare n characters
+ * _strncmp - Compare n characters of two strings
  */
 int _strncmp(const char *s1, const char *s2, size_t n)
 {
     size_t i;
+
     for (i = 0; i < n && s1[i] && s2[i]; i++)
     {
         if (s1[i] != s2[i])
@@ -41,32 +49,79 @@ int _strncmp(const char *s1, const char *s2, size_t n)
     }
     if (i < n)
         return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-    return 0;
+    return (0);
 }
 
 /**
- * trim_spaces - trim leading and trailing spaces
+ * _memcpy - Copies n bytes from src to dest
  */
-char *trim_spaces(char *str)
-{
-    char *end;
-    while (*str && (*str == ' ' || *str == '\t'))
-        str++;
-    end = str + _strlen(str) - 1;
-    while (end > str && (*end == ' ' || *end == '\t'))
-        *end-- = '\0';
-    return str;
-}
-
-/**
- * _memcpy - copy memory
- */
-void _memcpy(void *dest, void *src, size_t n)
+void *_memcpy(void *dest, const void *src, size_t n)
 {
     size_t i;
     char *d = dest;
-    char *s = src;
+    const char *s = src;
 
     for (i = 0; i < n; i++)
         d[i] = s[i];
+    return (dest);
+}
+
+/**
+ * _trim_spaces - Trim leading and trailing spaces from a string
+ */
+void _trim_spaces(char *str)
+{
+    char *end;
+    int len;
+
+    while (*str == ' ' || *str == '\t')
+        str++;
+
+    len = _strlen(str);
+    if (len == 0)
+    {
+        str[0] = '\0';
+        return;
+    }
+
+    end = str + len - 1;
+    while (end > str && (*end == ' ' || *end == '\t'))
+        end--;
+
+    *(end + 1) = '\0';
+}
+
+/**
+ * split_line - Splits a line into tokens (arguments)
+ */
+char **split_line(char *line)
+{
+    int bufsize = 64, pos = 0;
+    char **tokens = malloc(bufsize * sizeof(char *));
+    char *token;
+
+    if (!tokens)
+    {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(line, " \t\r\n");
+    while (token != NULL)
+    {
+        tokens[pos++] = _strdup(token);
+        if (pos >= bufsize)
+        {
+            bufsize *= 2;
+            tokens = realloc(tokens, bufsize * sizeof(char *));
+            if (!tokens)
+            {
+                perror("realloc");
+                exit(EXIT_FAILURE);
+            }
+        }
+        token = strtok(NULL, " \t\r\n");
+    }
+    tokens[pos] = NULL;
+    return (tokens);
 }
