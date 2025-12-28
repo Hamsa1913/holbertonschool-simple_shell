7c61a7c3004170669a13908ddb6966366c6668a7
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-12-28 06:52:49 +0000

    review

--- simple_shell.h
+++ simple_shell.h
@@ -2,12 +2,11 @@
 #define SIMPLE_SHELL_H
 
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
-#include <sys/wait.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 extern char **environ;
 
@@ -16,14 +15,13 @@ int hsh_loop(void);
 
 /* path functions */
 char *find_path(char *cmd);
-char *build_path(char *dir, char *cmd);
+void trim_spaces(char *str);
 
 /* utility file functions becuse i cant used more than 5 functions */
 size_t _strlen(const char *s);
 char *_strdup(const char *s);
 int _strncmp(const char *s1, const char *s2, size_t n);
-char trim_spaces(char *str);
-void *_memcpy(void *dest, const void *src, size_t n); 
+void *_memcpy(void *dest, const void *src, size_t n);
 
 /*split line into argument*/
 char **split_line(char *line);
