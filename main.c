2eb843957f2e799ee2c3d96b4f2725b352a001eb
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-12-28 07:37:13 +0000

    fixing main

--- main.c
+++ main.c
@@ -2,18 +2,11 @@
 
 /**
  * main - entry point 
- * @ac: argument count
- * @av: argument vector
  *
  * Return: 0 on success
  */
-int main(int ac, char **av)
+int main(void)
 {
-	(void)ac;
-	(void)av;
-
 	hsh_loop();
-
 	return (0);
 }
-
