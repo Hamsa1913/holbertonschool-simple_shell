b1d6647b00b777fb6a369a9f078b9e17689eca6b
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-12-29 08:33:51 +0000

    trying to set s path for an empty string

--- hsh.c
+++ hsh.c
@@ -1,79 +1,64 @@
 #include "simple_shell.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
 #include <sys/wait.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <string.h>
 
 extern char **environ;
 
 /**
- * hsh_loop - main shell loop
- * Return: 0
+ * hsh_loop - main shell execution loop
+ *
+ * Return: 0 on success
  */
 int hsh_loop(void)
 {
-	char *line = NULL, *cmd_path = NULL;
+	char *line = NULL;
 	size_t len = 0;
 	ssize_t read;
 	char **argv;
+	char *cmd_path;
 	pid_t pid;
-	int interactive = isatty(STDIN_FILENO);
 
-	while (1)
+	while ((read = getline(&line, &len, stdin)) != -1)
 	{
-		if (interactive)
-			write(STDOUT_FILENO, "$ ", 2);
-
-		read = getline(&line, &len, stdin);
-		if (read == -1)
-			break;
-
-		if (line[read - 1] == '\n')
-			line[read - 1] = '\0';
-
-		if (*line == '\0')
-			continue;
-
-		argv = split_line(line);
-		if (!argv || !argv[0])
+		if (read > 1)
 		{
+			argv = split_line(line);
+			if (argv && argv[0])
+			{
+				/* Resolve command path */
+				if (strchr(argv[0], '/'))
+					cmd_path = argv[0];
+				else
+					cmd_path = find_path(argv[0]);
+
+				/* Command not found */
+				if (!cmd_path)
+				{
+					write(STDERR_FILENO, "./hsh: 1: ", 10);
+					write(STDERR_FILENO, argv[0], strlen(argv[0]));
+					write(STDERR_FILENO, ": not found\n", 13);
+					free_argv(argv);
+					free(line);
+					exit(127);
+				}
+
+				/* Execute command */
+				pid = fork();
+				if (pid == 0)
+				{
+					execve(cmd_path, argv, environ);
+					exit(1);
+				}
+				wait(NULL);
+			}
 			free_argv(argv);
-			continue;
 		}
-
-		/* Resolve command BEFORE fork */
-		if (strchr(argv[0], '/'))
-			cmd_path = argv[0];
-		else
-			cmd_path = find_path(argv[0]);
-
-		if (!cmd_path)
-		{
-			write(STDERR_FILENO,
-			      "execve: No such file or directory\n", 34);
-			free_argv(argv);
-			continue;
-		}
-
-		pid = fork();
-		if (pid == 0)
-		{
-			execve(cmd_path, argv, environ);
-			perror("execve");
-			exit(EXIT_FAILURE);
-		}
-		else
-			wait(NULL);
-
-		if (cmd_path != argv[0])
-			free(cmd_path);
-
-		free_argv(argv);
 	}
-
 	free(line);
 	return (0);
 }
+
