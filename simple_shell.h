5dbecc361e5a3ba700fe138a1389dd34596d37c5
Hamsa1913 <Hamsa12021@gmail.com>
2025-12-30 07:14:02 +0000

    Add header file with function prototypes

--- simple_shell.h
+++ simple_shell.h
@@ -1,32 +1,14 @@
 #ifndef SIMPLE_SHELL_H
 #define SIMPLE_SHELL_H
 
-#include <stddef.h>
-#include <unistd.h>
-#include <unistd.h>
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <sys/wait.h>
 
-
-
-/* Global variable for environment */
 extern char **environ;
 
-/* Main loop */
-int hsh_loop(void);
-
-/* String and memory utilities */
-size_t _strlen(const char *s);
-char *_strdup(const char *s);
-void *_memcpy(void *dest, const void *src, size_t n);
-char *trim_spaces(char *str);
-
-/* Parsing and freeing */
 char **split_line(char *line);
-void free_argv(char **argv);
+void execute(char **args);
 
-/* PATH resolution */
-char *find_path(const char *command);
-#endif /* SIMPLE_SHELL_H */
-
+#endif
