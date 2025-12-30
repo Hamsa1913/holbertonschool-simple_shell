4c8dfa4a69355dbb44ba367edcafcad7eeb69d3a
Hamsa1913 <Hamsa12021@gmail.com>
2025-12-30 07:08:58 +0000

    Add execute function to run external commands

--- hsh.c
+++ hsh.c
@@ -1,79 +1,20 @@
 #include "simple_shell.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/wait.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-
-extern char **environ;
 
 /**
- * hsh_loop - main shell loop
- * Return: 0
+ * execute - Executes a command using fork and execve
+ * @args: Command arguments
  */
-int hsh_loop(void)
+void execute(char **args)
 {
-        char *line = NULL, *cmd_path = NULL;
-        size_t len = 0;
-        ssize_t read;
-        char **argv;
-        pid_t pid;
-        int interactive = isatty(STDIN_FILENO);
+    pid_t pid;
 
-        while (1)
-        {
-                if (interactive)
-                        write(STDOUT_FILENO, "$ ", 2);
-
-                read = getline(&line, &len, stdin);
-                if (read == -1)
-                        break;
-
-                if (line[read - 1] == '\n')
-                        line[read - 1] = '\0';
-
-                if (*line == '\0')
-                        continue;
-
-                argv = split_line(line);
-                if (!argv || !argv[0])
-                {
-                        free_argv(argv);
-                        continue;
-                }
-                 /* Resolve command BEFORE fork */
-                if (strchr(argv[0], '/'))
-                        cmd_path = argv[0];
-                else
-                        cmd_path = find_path(argv[0]);
-
-                if (!cmd_path)
-                {
-                        write(STDERR_FILENO, "./hsh: 1: ", 10);
-			write(STDERR_FILENO, argv[0], strlen(argv[0]));
-			write(STDERR_FILENO, ": not found\n", 13);
-
-			free_argv(argv);
-			continue;
-		}
-
-		pid = fork();
-		if (pid == 0)
-		{
-			execve(cmd_path, argv, environ);
-			exit(127);
-		}
-		else
-			wait(NULL);
-
-		if (cmd_path != argv[0])
-			free(cmd_path);
-
-		free_argv(argv);
-	}
-
-	free(line);
-	return (0);
+    pid = fork();
+    if (pid == 0)
+    {
+        if (execve(args[0], args, environ) == -1)
+            perror("Error");
+        exit(EXIT_FAILURE);
+    }
+    else
+        wait(NULL);
 }
