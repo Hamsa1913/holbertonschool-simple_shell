047b0e5a41eec59ec19ab38445dd833087c55d0d
Hamsa1913 <Hamsa12021@gmail.com>
2025-12-30 07:37:13 +0000

    Update execute function to return correct exit status; exit 2 if command fails

--- hsh.c
+++ hsh.c
@@ -1,20 +1,35 @@
 #include "simple_shell.h"
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * execute - Executes a command using fork and execve
  * @args: Command arguments
+ * Return: status of child process
  */
-void execute(char **args)
+int execute(char **args)
 {
     pid_t pid;
+    int status;
 
     pid = fork();
     if (pid == 0)
     {
         if (execve(args[0], args, environ) == -1)
-            perror("Error");
-        exit(EXIT_FAILURE);
+        {
+            perror(args[0]); /* print error on stderr */
+            exit(2);         /* exit with status 2 if command fails */
+        }
+        exit(0);
     }
     else
-        wait(NULL);
+    {
+        wait(&status);
+        if (WIFEXITED(status))
+            return WEXITSTATUS(status);
+    }
+
+    return 0;
 }
